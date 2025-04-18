35a1f908ccbbaaae367bc10f38644a0f79e87f1f
Venkatesh <venkatvelladurai@gmail.com>
2025-04-18 19:46:15 +0530

    Revert "Add files via upload"

--- expense-backend/expense-backend/src/main/java/com/expense_management_backend/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.expense_management_backend.repository;
-
-import com.expense_management_backend.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
